6e38c9ea9a6c58140c86555cf206fc77ceca2fb9
Van4o <aslanyahya91@gmail.com>
2023-09-26 20:39:56 +0300

    incarcare exemplu pe github

---
+++ src/lectie/TipVariabila.java
@@ -0,0 +1,39 @@
+package lectie;
+
+public class TipVariabila {
+    public static void main(String[] args) {
+//        int number = 8;
+//        float numarFlotant = 8.99f;
+//        char Character = 'J';
+//        boolean b = true;
+//        String text = "Hello Java";
+//        System.out.println(number);
+//        System.out.println(numarFlotant);
+//        System.out.println(Character);
+//        System.out.println(b);
+//
+//        String prenume = "Mihai ";
+//        String numeFamilie = "Petrescu";
+//        String numeIntreg = prenume + numeFamilie;
+//        System.out.println(numeIntreg);
+//
+//        int x = 3;
+//        int y = 5;
+//        System.out.println(x + y);
+        //declararea a 3 variabile a tipului int
+        int a, b, c;
+        //declararea a 3 variabile a tipului int cu
+        //initializarea variabilelor d si f
+        int d = 5, e, f = 8;
+        //initializarea variabilei z
+        byte z = 22;
+        //initia;izarea variabilei pi si
+        //atribuirea valorii aproximative valorii pi
+        double pi = 3.14159265;
+        //atribuirea simbolului 'x' variabilei x
+        char x = 'x';
+
+
+
+    }
+}
